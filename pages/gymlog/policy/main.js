ea3d29f102db561b31b393e2ee41a8198f148e9e
hhh67 <hhio.67.2020@gmail.com>
2024-04-02 22:12:12 +0900

    ポリシーはpolicy直下に移動

--- pages/gymlog/policy/main.js
+++ pages/gymlog/policy/main.js
@@ -3,7 +3,7 @@ window.addEventListener('load', () => {
     const isDarkMode = darkModeMediaQuery.matches;
 
     const logo = document.getElementById('h1Logo')
-    if (logo && isDarkMode) logo.src = "../../public/gymlog/logo_dark.png"
+    if (logo && isDarkMode) logo.src = "../../../public/gymlog/logo_dark.png"
 
     const lastUpdatedDate = document.getElementById('lastUpdatedDate')
     if (lastUpdatedDate) {
