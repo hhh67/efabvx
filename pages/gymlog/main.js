ebe84e1761193540e512dc64f6283b9e5f5bf155
hhh67 <hhio.67.2020@gmail.com>
2024-04-03 20:24:02 +0900

    /lpを削除して/gymlog直下に移動

--- pages/gymlog/main.js
+++ pages/gymlog/main.js
@@ -3,7 +3,13 @@ window.addEventListener('load', () => {
     const isDarkMode = darkModeMediaQuery.matches;
 
     const logo = document.getElementById('h1Logo')
-    if (logo && isDarkMode) logo.src = "../../public/gymlog/logo_dark.png"
+    const appStoreLogo = document.getElementById('appStoreLogo')
+    const xLogo = document.getElementById('xLogo')
+    if (logo && isDarkMode) {
+        logo.src = "../../public/gymlog/logo_dark.png"
+        appStoreLogo.src = "../../public/gymlog/app_store_logo_dark.svg"
+        xLogo.src = "../../public/gymlog/x_logo_dark.png"
+    }
 
     const lastUpdatedDate = document.getElementById('lastUpdatedDate')
     if (lastUpdatedDate) {
