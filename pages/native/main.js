142c7cec7ac6f9953e0ebfa68cf62e75d3a8dee8
hhh67 <hhio.67.2020@gmail.com>
2024-04-04 01:25:50 +0900

    作ったアプリ一覧ページ作成

---
+++ pages/native/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const isDarkMode = darkModeMediaQuery.matches;
+
+    const appStoreLogo = document.getElementsByClassName('app-store-logo')
+    if (appStoreLogo && isDarkMode) {
+        Array.prototype.forEach.call(appStoreLogo, (el) => {
+            el.src = "../../public/app_store_logo_dark.svg"
+        })
+    }
+
+    const params = new URLSearchParams(location.search)
+    switch (params.get('from')) {
+        case 'gymlogapp':
+            const gymlogCard = document.getElementById('appCardGymlog')
+            gymlogCard.style.display = 'none'
+            break;
+        case 'qiitareaderapp':
+            const qiitaReaderCard = document.getElementById('appCardQiitaReader')
+            qiitaReaderCard.style.display = 'none'
+            break;
+        default:
+            break;
+    }
+})
