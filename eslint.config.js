0e7acd9238f686218076213d8206c39f8532fd7a
hhh67 <hhio.67.2020@gmail.com>
2024-06-20 23:21:42 +0900

    bun eslint --init

---
+++ eslint.config.js
@@ -0,0 +1,82 @@
+import pluginTypeScript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import pluginPrettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import reactConfigRecommended from 'eslint-plugin-react/configs/recommended.js';
+
+export default [
+  {
+    ignores: ['./dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    plugins: {
+      '@typescript-eslint': pluginTypeScript,
+    },
+    rules: {
+      ...pluginTypeScript.configs['eslint-recommended'].rules,
+      ...pluginTypeScript.configs['recommended'].rules,
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        typescript: {},
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      import: pluginImport,
+    },
+    rules: {
+      ...pluginImport.configs['recommended'].rules,
+      ...reactConfigRecommended.rules,
+      ...pluginPrettier.rules,
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'import/no-unresolved': 'off',
+      'no-unused-vars': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: '{react,react-dom/**,react-router-dom}',
+              group: 'builtin',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: {
+            order: 'asc',
+          },
+          'newlines-between': 'never',
+        },
+      ],
+    },
+  },
+];
