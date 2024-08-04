adbae5ab9831440641e8d2df63b030a34f0e845d
Hideya Hoshino <hhio.67.2020@gmail.com>
2024-08-04 16:47:38 +0900

    viteの設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
-export default defineConfig(({}) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     checker({
       typescript: true,
     }),
   ],
+  server: {
+    host: mode === 'development' ? true : false,
+  },
   resolve: {
     alias: {
       '@': path.join(__dirname, 'src'),
