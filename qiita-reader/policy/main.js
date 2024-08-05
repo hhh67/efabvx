bf90b8bdcc0ee4880121f5e0309c3ee72d0ebc82
Hideya Hoshino <hhio.67.2020@gmail.com>
2024-08-05 22:35:34 +0900

    一時的に作成したページを削除

--- qiita-reader/policy/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const lastUpdatedDate = document.getElementById('lastUpdatedDate')
-    if (lastUpdatedDate) {
-        lastUpdatedDate.innerText = '最終更新: ' + new Date(document.lastModified).toLocaleString('ja-JP')
-    }
-})
