482fb10a6fde1338646af4e09bc93c7ee1856bf5
Hideya Hoshino <hhio.67.2020@gmail.com>
2024-08-05 22:25:54 +0900

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@ rm -rf docs
 
 npm run build 
 
+touch dist/CNAME
+echo "efabvx.com" > dist/CNAME
+
+rm -rf dist/qiita-reader
+mkdir dist/public
+cp public/qiita-reader dist/public/qiita-reader
+
 cp dist/index.html dist/404.html
 
 mv dist docs
